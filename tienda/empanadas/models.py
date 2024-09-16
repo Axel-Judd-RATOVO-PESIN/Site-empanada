54cf968c97d1001454d30fae09c9d0989fc58af0
aratovo <axel-judd.ratovo-pesin@universite-paris.saclay.fr>
2024-09-16 15:30:02 +0200

    Ajout des modification sur fichier models.py | creation des classes

--- tienda/empanadas/models.py
+++ tienda/empanadas/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model) :
+    #cle primaire, avec auto-increment
+    idIngredient = models.AutoField( primary_key = True )
+    #chaine de caractere de taille bornee
+    nomIngredient = models.CharField( max_length = 50 )
+    #version python du toString(), utilise par django dans ses interfaces
+    def __str__(self) :
+        return self.nomIngredient
+
+
+
+class Empanda(models.Model) :
+    #cle primaire, avec auto-increment
+    idEmpanada    = models.AutoField(    primary_key    = True )
+    #chaine de caractere de taille bornee
+    nomEmpanada   = models.CharField(   max_length      = 50   )
+    #nombre decimal, max 6 chiffres dont 2 apres la virgule
+    prix          = models.DecimalField( max_digits     = 6, decimal_places = 2 )
+    #version pytho, du toString(), utilise par django dans ses interfaces 
+    def __str__(self) :
+        return 'empanda'+self.nomEmpanada+' (prix:'+str(self.prix)+'€)'
+
