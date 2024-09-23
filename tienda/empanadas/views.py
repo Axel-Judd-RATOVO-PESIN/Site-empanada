47e04d1ebfb140b339b57740460c0ac0e34941d5
aratovo <axel-judd.ratovo-pesin@universite-paris.saclay.fr>
2024-09-23 16:07:29 +0200

    Ajout des modification fichier empanad.html | [ A FAIRE ]Finir remplir fichier empanadas.html

--- tienda/empanadas/views.py
+++ tienda/empanadas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from empanadas.models import Empanada
+
+def empanadas(request):
+    lesEmpanadas = Empanada.objects.all()
+    return render(request,'empanadas/empanadas.html',{'empanadas' : lesEmpanadas} )
+
