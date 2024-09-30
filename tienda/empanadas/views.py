9173c69be36d4e864b30a1b35e54825b3823067d
aratovo <axel-judd.ratovo-pesin@universite-paris.saclay.fr>
2024-09-30 16:04:14 +0200

    Ajout des modification | TP3 FINIS -> TP4 A COMMENCER

--- tienda/empanadas/views.py
+++ tienda/empanadas/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.shortcuts import render
 from empanadas.models import Empanada
+from empanadas.models import Ingredient
 
 def empanadas(request):
     lesEmpanadas = Empanada.objects.all()
     return render(request,'empanadas/empanadas.html',{'empanadas' : lesEmpanadas} )
 
+def ingredients(request):
+    lesIngredients = Ingredient.objects.all()
+    return render(request, 'empanadas/ingredients.html',{'ingredients' : lesIngredients} )
