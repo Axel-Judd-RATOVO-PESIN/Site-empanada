19452981cdcd58b38960be1d032ec94167bc3031
Axel Judd RATOVO PESIN <axelratovopesin@universite-paris-saclay.fr>
2024-10-13 17:09:29 +0200

    Ajout des modifications | Fin de l'EXO 2 -> FINIR D'AFFICHER LES INGREDIENTS ET QUANTITE

--- tienda/empanadas/views.py
+++ tienda/empanadas/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from empanadas.models import Empanada
 from empanadas.models import Ingredient
+from empanadas.models import Composition
 
 def empanadas(request):
     lesEmpanadas = Empanada.objects.all()
@@ -10,3 +11,21 @@ def empanadas(request):
 def ingredients(request):
     lesIngredients = Ingredient.objects.all()
     return render(request, 'empanadas/ingredients.html',{'ingredients' : lesIngredients} )
+
+def empanada(request, empanada_id) :
+	laEmpanada = Empanada.objects.get( idEmpanada= empanada_id)
+	compositions = Composition.objects.filter(empanada = empanada_id)
+
+	ingredients_list = []
+	for compo in compositions:
+		ingredients_list.append({
+			'Ingredient' : compo.ingredient.nomIngredient,
+			'Quantité' : compo.quantite
+		})
+	
+	context = {
+		'empanada': laEmpanada,
+		'ingredients_list': ingredients_list,
+	}	
+
+	return render( request, 'empanadas/empanada.html', context )
